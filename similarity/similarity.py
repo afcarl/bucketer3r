6201761739b86d10e76ee9f1481d77aa9d855267
Matthew Ruttley <mruttley@mozilla.com>
2015-05-18 20:01:14 -0400

    Basic HTML cruncher added

--- similarity/similarity.py
+++ similarity/similarity.py
@@ -13,15 +13,31 @@ from re import findall
 from os import listdir
 from collections import defaultdict, Counter
 from math import sqrt
+from datetime import datetime
+from pdb import set_trace
 
 from pymongo import MongoClient
 from stopwords import stopwords
+from bs4 import BeautifulSoup
 
 #setup
 stopwords = stopwords()
 directory = '/Users/mruttley/Documents/2015-04-22 AdGroups/Bucketerer/data_crunching/alexa_data/'
+html_directory = "/Users/mruttley/Documents/2015-05-11 StatCounter HTML Crawler/html/"
+ranking_directory = "/Users/mruttley/Documents/2015-04-22 AdGroups/Bucketerer/data_crunching/ranking_files/"
 
 #Auxiliary functionality
+
+def get_ranking():
+	"""Gets a ranking of domains"""
+	timestamp = datetime.strftime(datetime.now(), '%Y-%m-%d')
+	filename = timestamp+"top-1m.csv"
+	domains = []
+	with open(ranking_directory + filename) as f:
+		for line in f:
+			domain = line[:-1].split(",")[1].lower()
+			domains.append(domain)
+	return domains
 
 def create_connection():
 	"""Creates a mongodb connection"""
@@ -125,6 +141,75 @@ def generate_domain_file():
 			if n % 5000 == 0:
 				print "Processed {0}".format(n)
 
+def html_test(domain):
+	with open(html_directory+domain+".html") as f:
+		soup = BeautifulSoup(f.read())
+		description = soup.findAll('meta', attrs={'name':'description', 'content':True})
+		print description
+		if description:
+			print description[0]['content']
+
+def parse_html_file(domain):
+	"""Extracts information about a page from its HTML"""
+	
+	info = {}
+	with open(html_directory+domain+'.html') as f:
+		#import the file
+		soup = BeautifulSoup(f.read())
+		
+		#description
+		description = soup.findAll('meta', attrs={'name':'description', 'content':True})
+		if description:
+			info['meta_description'] = description[0]['content']
+		
+		#title
+		if soup.title:
+			info['page_title'] = soup.title.text
+		
+		#keywords
+		keywords = soup.findAll('meta', attrs={'name':'keywords', 'content':True})
+		if keywords:
+			keywords = keywords[0]['content']
+			if "," in keywords:
+				keywords = [x.strip() for x in keywords.split(",")]
+				info['meta_keywords'] = keywords
+	
+	return info
+
+def generate_html_files():
+	"""Creates files from the meta description, title, keywords. In future maybe page content too."""
+	
+	c = create_connection()
+	
+	files = set(listdir(html_directory))
+	for n, domain in enumerate(get_ranking()):
+		if (domain + ".html") in files:
+			try:
+				info = parse_html_file(domain)
+				#now update database record
+				entry = c['domains'].find_one({'domain':domain.replace(".", "#")}, {'_id':1})
+				if entry:
+					c['domains'].update(
+						{
+							'_id': entry['_id']
+						},
+						{
+						'$set': {
+							'html': info
+						}
+					})
+				else:
+					c['domains'].insert({
+						'domain': domain.replace(".", "#"),
+						'html': info
+					})
+			
+			except Exception, e:
+				print Exception, e
+			
+			print n, domain, len(info)
+			
+
 #Testing and analysis
 
 def test(site):
@@ -205,6 +290,67 @@ def find_by_site_description(starter_site, index):
 def find_by_similar_sites(starter_site):
 	"""Searches similarsites crawled data"""
 
-def find_by_html():
-	"""Searches and ranks by page HTML"""
+def find_by_html(starter_site):
+	"""Searches and ranks by page HTML attributes"""
+	
+	c = create_connection()
+	results = [["", 0, ""] for x in range(50)] #ranking
+	
+	#get the description of the starter site
+	domain = starter_site.replace('.', '#')
+	entry = c['domains'].find_one({'html.meta_description':{'$exists':True}, 'domain':domain}, {'html.meta_description':1, 'domain':1})
+	starter_desc = entry['html']['meta_description']
+	starter_desc = tokenize_clean(starter_desc) #clean it and tokenize it
+	print starter_desc
+	starter_vector = Counter(starter_desc) #vectorize it
+	
+	scanned = 0
+	for entry in c['domains'].find({'html.meta_description':{'$exists':True}}, {'html.meta_description':1, 'domain':1}):
+		scanned += 1
+		domain = entry['domain']
+		desc = entry['html']['meta_description']
+		
+		kw_vector = Counter(tokenize_clean(desc)) #vectorize desc
+		cosim = opt_cosine_similarity(starter_vector, kw_vector)
+		if cosim:
+			if cosim > results[-1][1]:
+				for x in range(len(results)):
+					if cosim > results[x][1]:
+						results = results[:x] + [[domain, cosim, desc]] + results[x:]
+						results = results[:50]
+						break
+	
+	print scanned
+
+	for x in range(len(results)):
+		if results[x][1] == 0:
+			results = results[:x] #cut off blank tail of results if present
+			break
+
+	for x in results[:10]:
+		print x
+	
+	#return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
 	
